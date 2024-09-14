cc2b1e272483b0006e75c0a299284b3832ecd2e8
abuMuhsinCoda <139397387+abuMuhsinCoda@users.noreply.github.com>
2024-09-14 23:58:03 +0100

    First commit

--- flights/models.py
+++ flights/models.py
@@ -19,8 +19,8 @@ class Flight(models.Model):
     
     # Addition from lecture 7 Testing and CI-CD
     def is_valid_flight(self):
-        return self.origin != self.destination or self.duration >= 0
-        # return self.origin != self.destination and self.duration >= 0
+        # return self.origin != self.destination or self.duration >= 0
+        return self.origin != self.destination and self.duration >= 0
 
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
